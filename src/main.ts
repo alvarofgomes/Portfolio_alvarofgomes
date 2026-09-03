4a44af55a2239e536a80e70b315d2e8e77b74ec7
alvarofgomes <alvarogomes098@gmail.com>
2026-09-03 20:23:49 -0300

    chore: mover CSS global para src/styles/ (base.css + layout.css), alinhando com o padrão do Rotina Semanal

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import "./style.css";
+import "./styles/index.css";
 
 createApp(App).use(router).mount("#app");
